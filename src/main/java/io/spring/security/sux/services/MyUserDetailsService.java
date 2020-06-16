a3af69ffdccd8a98c3ab1ad6f4463a0d1be22cc0
Abraham Juárez <AjSux@outlook.com>
2020-06-16 17:35:35 -0500

    Nombre del Proyecto: Spring Security JPA
    Descripción: Se agrega la lógica de acceso a datos, para ir a consultar a la base de datos cuando se accede mediante las url del controller.

--- src/main/java/io/spring/security/sux/services/MyUserDetailsService.java
+++ src/main/java/io/spring/security/sux/services/MyUserDetailsService.java
@@ -1,17 +1,34 @@
 package io.spring.security.sux.services;
 
-import io.spring.security.sux.models.MyUserDetails;
+import io.spring.security.sux.models.User;
+import io.spring.security.sux.repositories.UserRepository;
+import io.spring.security.sux.services.MyUserDetails;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service("myUserDetailsService")
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    @Qualifier("userRepository")
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
     }
+
+
 
 }
